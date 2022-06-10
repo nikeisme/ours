85d9b7dfcad54006eafdb292b7debd5bf99272ff
unknown <nikeisme827@gmail.com>
2022-06-10 16:53:36 +0900

    Initial commit

---
+++ src/main/java/dev/ours/ours/OursApplication.java
@@ -0,0 +1,13 @@
+package dev.ours.ours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ours/ours/OursApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ours.ours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
